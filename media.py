d75219820ca71f97f01a97e408ef60c25e7f5283
gabriellyandelina <gabriellysantos28032007@gmail.com>
2023-08-02 20:23:29 -0300

    criado um arquivo que calcula a área de um círuo de raio 30, e editar o arquivo da média de 3 números

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
-nun01=int(input('Digite um valor'))
-nun02=int(input('Digite um segundo valor'))
-result=(nun01+nun02)/2
+nun01=30
+nun02=90
+nun03=75
+result=(nun01+nun02+nun03)/3
+
 print('A média desses números é',result)
 #commit
 #git add : adicione um pacote
---
+++ área.py
@@ -0,0 +1,3 @@
+raio=30
+área=3,14*30**2
+print(área)
