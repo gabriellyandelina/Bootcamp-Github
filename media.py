849dbacb2486f1530dff4d9459c82f554d77d29e
unknown <Administrador@gi02lab1017.integrado.int>
2023-08-02 19:44:45 -0300

    criado codigo para calculo de média

---
+++ media.py
@@ -0,0 +1,8 @@
+nun01=int(input('Digite um valor'))
+nun02=int(input('Digite um segundo valor'))
+result=(nun01+nun02)/2
+print('A média desses números é',result)
+#commit
+#git add : adicione um pacote
+#git commit -n " "
+#git dush
